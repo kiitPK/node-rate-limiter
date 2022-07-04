c52d71ca72f0a7a6840062e9ac49a02d0d49fb87
kiitPK <prakashsingh184@gmail.com>
2022-07-04 18:40:27 +0530

    initial rate limiter express rate limiter

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const rateLimit = require("express-rate-limit");
+
+const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+app.use(limiter);
+
+app.use("/", limiter, (req, res) => {
+  res.send("hi");
+});
+
+app.listen(5000, () => {
+  console.log("hi");
+});
